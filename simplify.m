bb129e73579ea248aacb2f20cb213504572db77b
Sam Berning <shberning@gmail.com>
2016-04-21 00:22:00 -0400

    Added files via upload

--- simplify.m
+++ simplify.m
@@ -44,6 +44,9 @@ while ~complete
             elseif (i-1) > 0 && any(str(i-1) == funcs)    % replaces parentheses used to denote functions
                 str(i) = '[';                             % with square brackets so they will be treated
                 str(i + nxtclose(1)) = ']';               % differently
+            elseif (i-1) > 0 && str(i-1) == '^'           % replaces parentheses around exponents with curly
+                str(i) = '{';                             % braces so they will be treated differently as well
+                str(i + nxtclose(1)) = '}';               % this is for latexify, mainly
             else
                 str(i) = char(minchar + lvl);               % changes parentheses to the ASCII char
                 str(i + nxtclose(1)) = char(minchar + lvl); % corresponding to the current level
@@ -63,94 +66,93 @@ end
 % that is only composed of numbers and operators and can be easily
 % evaluated by doing a simple operation
 
-complete = false;   % whether or not this process is complete
-lvl = 0;            % level of the parentheses, this number is added to minchar to choose which char will
-% replace this level of parentheses
-
-while ~complete
-    pars = strfind(str,char(minchar + lvl));        % finds all of the parentheses on this level
-    nxtpars = strfind(str,char(minchar + lvl + 1)); % finds all of the parentheses on the next level
-    if isempty(pars) && isempty(nxtpars)            % if there are none of either we must be done with this
-        complete = true;                            % much simplification, at least
-        continue;
-    else                                            % otherwise, we need to do some stuff
-        jmax = length(pars)/2;                      % every odd parenthesis on this level will be an open
-        for j = 1:jmax                              % parenthesis, so we can jump through all of the
-            start = pars(2*j-1) + 1;                % parentheses on this level by using 2*j-1 to get all of
-            stop = pars(2*j) - 1;                   % the odd-numbered parentheses and 2*j to get the evens
-            if isSimple(str(start:stop))            % tests to see if the string is evaluable mathematically
-                str = [str(1:start-2), evalString(str(start:stop)),...  % (that is, it contains only numbers)
-                    str(stop+2:end)];                                   % if it is, it evaluates it
-                break;                              % since the string is a different length than it was at
-                                                    % the beginning of the for loop, we need to break the
-                                                    % for loop and take the length of the new string to continue
-            end
+for i = 0:lvl
+    pars = strfind(str,char(minchar+i));        % finds all of the parentheses on this level
+    jmax = length(pars)/2;                      % every odd parenthesis on this level will be an open
+    for j = 1:jmax                              % parenthesis, so we can jump through all of the
+        start = pars(2*j-1) + 1;                % parentheses on this level by using 2*j-1 to get all of
+        stop = pars(2*j) - 1;                   % the odd-numbered parentheses and 2*j to get the evens
+        if isSimple(str(start:stop))            % tests to see if the string is easily evaluable mathematically
+            str = [str(1:start-2), evalString(str(start:stop)),...  % (that is, it contains only numbers)
+                str(stop+2:end)];                                   % if it is, it evaluates it
         end
-    end
-    if isempty(j) || j >= jmax                      % if either the counter on the for loop is empty or if
-        lvl = lvl + 1;                              % it is at its maximum value, we move on to the next
-                                                    % level of parentheses
     end
 end
 
 %% removes unnecessary parentheses
+% derive.m outputs expressions that are absolutely riddled with
+% parentheses. here we try to make sense of that.
 
-for j = 0:lvl
-    pars = strfind(str,char(minchar+j));
-    for i = 1:length(pars)/2
-        if pars(2*i-1) == 1
+for j = 0:lvl                               % max level of parentheses is stored in lvl, so we can now use a for loop
+    pars = strfind(str,char(minchar+j));    % this finds all of the parentheses on this level
+    for i = 1:length(pars)/2                % then we can easily index through pairs of parentheses by using a for loop
+        if pars(2*i-1) == 1                 % and finding all of the odd- and even- numbered parentheses
+                                            % (odds are open parentheses, evens are closed parentheses)
             if any(str(pars(2*i-1):pars(2*i)) == '/') && ~(pars(2*i) == length(str) || any(str(pars(2*i)+1) == '*+-/'))
-                continue;
+                continue;                   % checks to see if there is a division operation going on. if there is,
+                                            % and if it is not the only expression, we keep the parentheses
             else
-                str(pars(2*i-1)) = '!';
-                str(pars(2*i)) = '!';
+                str(pars(2*i-1)) = '!';     % if it is the only expression, though, we get rid of the parentheses
+                str(pars(2*i)) = '!';       % notice we actually replace the parentheses with exclamation points.
+                                            % if we didn't do this, the string would change length on each iteration
+                                            % and we would have another problem. the code removes the exclamation
+                                            % points later. 
             end
-        elseif pars(2*i) - pars(2*i-1) < 3
-            str(pars(2*i-1)) = '!';
+        elseif pars(2*i) - pars(2*i-1) < 3  % if there is only one character between the two parentheses, we get rid
+            str(pars(2*i-1)) = '!';         % of the parentheses
             str(pars(2*i)) = '!';
-        elseif any(str(pars(2*i-1):pars(2*i)) == '/')
-            continue;
-        elseif str(pars(2*i-1)-1) == '+'
-            str(pars(2*i-1)) = '!';
+        elseif any(str(pars(2*i-1):pars(2*i)) == '/') % if there is a division operation inside, we
+            continue;                                 % keep the parentheses
+        elseif str(pars(2*i-1)-1) == '+'                % otherwise if it comes after a plus, we get rid of the
+            str(pars(2*i-1)) = '!';                     % parentheses
             str(pars(2*i)) = '!';
-        elseif any(str(pars(2*i-1):pars(2*i)) == '+')
+        elseif any(str(pars(2*i-1):pars(2*i)) == '+')   % if it's anything besides a plus, however, and there is a plus
+            continue;                                   % or minus inside, we keep the parentheses in case it is a 
+        elseif any(str(pars(2*i-1):pars(2*i)) == '-')   % times or subtraction operation
             continue;
-        elseif any(str(pars(2*i-1):pars(2*i)) == '-')
-            continue;
-        elseif any(str(pars(2*i-1)-1) == '*-')
-            str(pars(2*i-1)) = '!';
+        elseif strcmp(str(pars(2*i-1)-1),'/')           % if the parentheses come after a division operation, 
+            continue;                                   % we keep the parentheses
+        elseif strcmp(str(pars(2*i)+1),'^')             % if the parentheses are raised to an exponent, we also
+            continue;                                   % keep the parentheses
+        else
+            str(pars(2*i-1)) = '!';                     % otherwise we pitch them
             str(pars(2*i)) = '!';
         end
     end
 end
 
 %% turns characters back into parentheses
+% in order to help the user read this string, we need to make our weird
+% characters back into parentheses and delete all of the exclamation
+% points.
 
-for l = 0:lvl
-    pars = strfind(str,char(minchar+l));
+for l = 0:lvl                               % goes through all levels of parentheses
+    pars = strfind(str,char(minchar+l));    % finds the parentheses on that level
     for i = 1:(length(pars)/2)
-        start = pars(2*i - 1);
+        start = pars(2*i - 1);              % goes through all of the odds and evens
         stop = pars(2*i);
-        str(start) = '(';
-        str(stop) = ')';
+        str(start) = '(';                   % and changes odds to open parentheses
+        str(stop) = ')';                    % and evens to closed parentheses
     end
 end
-str = strrep(str,'[','(');
+str = strrep(str,'[','(');                  % finally, we replace square brackets with parentheses
 str = strrep(str,']',')');
-str = strrep(str,'!','');
+str = strrep(str,'!','');                   % and eliminate any exclamation points
 
 %% evaluates any unevaluated expressions
+% generally at this point there are some expressions such as 4*2*x that
+% could easily be simplified to 8*x. This is what we do here.
 
-ops = '*/+-';
+ops = '*/+-';                           % sets up the proper order of operations
 
-for i = 1:length(ops)
-    for j = 1:length(str)
+for i = 1:length(ops)                   % runs through each operation one at a time
+    for j = 1:length(str)               % goes through the whole string trying to find the current operation
         if strcmp(str(j),ops(i))
-            if isDigit(str(j-1)) && isDigit(str(j+1))
-                firstnum = j-1;
-                secndnum = j+1;
-                for k = 2:j
-                    if j-k <= 0 && j+k > length(str)
+            if ~(j == 1) && isDigit(str(j-1)) && isDigit(str(j+1))  % if the operation isn't the first char in the
+                firstnum = j-1;                                     % string, and the char on either side is a digit
+                secndnum = j+1;                                     % congrats! we can operate on it.
+                for k = 2:j                             % reads forward and back to make sure that the multipliers are
+                    if j-k <= 0 && j+k > length(str)    % not multi digit numbers
                         break;
                     elseif j-k <= 0
                         if isDigit(str(j+k))
@@ -171,51 +173,104 @@ for i = 1:length(ops)
                         break;
                     end
                 end
-                newExp = evalString(str(firstnum:secndnum));
-                str = [str(1:firstnum-1),...
-                    blanks(((secndnum+1)-(firstnum-1))-length(newExp)),...
-                    newExp, str(secndnum+1:end)];
+                newExp = evalString(str(firstnum:secndnum));                % then if it is evaluable, we evaluate it,
+                str = [str(1:firstnum-1),...                                % making sure to add in an appropriate
+                    blanks(((secndnum+1)-(firstnum-1))-length(newExp)),...  % number of spaces such that the string
+                    newExp, str(secndnum+1:end)];                           % stays the same length
             end
         end
     end
-    str = strrep(str,' ','');
+    str = strrep(str,' ','');                                               % then we get rid of all of the spaces
 end
 
-%% handles *0
+%% handles *0, 0*, and 0/
+% these strings nullify whole expressions, so we can scan to see how long
+% they propogate. In general, they go until there is a term that is added
+% or subtracted, though that's not how the scanning works
 
-tzs = strfind(str,'*0');
-
-if ~isempty(tzs)
-    plusses = strfind(str,'+');
-    minuses = strfind(str,'-');
-    pms = sort([plusses, minuses]);
-    if isempty(pms)
-        str = '0';
+% runs while any '*0' is left in the string
+while(any(strfind(str,'*0')))
+pos = strfind(str,'*0');
+pos = pos(length(pos));
+start = pos-1;
+while(true) % scans backwards for end of expression nullified
+    if(start == 0)% exits at start of string
+        start = 1;
+        break;
+    end
+    % keep going if find any of these characters
+    if(any(str(start)=='1234567890x*'))
+        start = start-1;
+        continue;
+    % scan backwards to the matching open paren
+    elseif(str(start) == ')')
+        level = 1;
+        while(level > 0)
+            start = start-1;
+            if(str(start) == ')')
+                level = level + 1;
+            elseif(str(start) == '(')
+                level = level - 1;
+            end
+        end
+        start = start-1;
+        continue;
     else
-        for j = 1:length(tzs)
-            for k = 1:length(pms)
-                if tzs(j) < pms(k) && k == 1
-                    str = ['0',str(pms(k):end)];
-                elseif tzs(j) > pms(k) && k == length(pms)
-                    str = [str(1:pms(k)),'0'];
-                elseif k ~= 1 && k ~= length(pms) && tzs(j) > pms(k-1) && tzs(j) < pms(k)
-                    str = [str(1:pms(k-1)),'0',str(pms(k):end)];
-                end
+        start = start+1; %move forward if character not valid
+    end
+    break;    
+end
+% removes scanned portion from string
+str = [str(1:start-1),str(pos+1:length(str))];
+end
+
+% keeps running if any '0*' or '0/' are in the string
+while(any(strfind(str,'0*'))|| any(strfind(str,'0/')))
+pos = [strfind(str,'0*'),strfind(str,'0/')];
+pos = pos(length(pos));
+ending = pos+1;
+while(true)
+    if(ending == length(str)+1) % ends selection if at end of string
+        ending = length(str);
+        break;
+    end
+    % continues past any of these characters
+    if(any(str(ending)=='1234567890x*/')) 
+        ending = ending+1;
+        continue;
+    % scan backwards to the matching close paren
+    elseif(str(ending) == '(')
+        level = 1;
+        while(level > 0)
+            ending = ending+1;
+            if(str(ending) == '(')
+                level = level + 1;
+            elseif(str(ending) == ')')
+                level = level - 1;
             end
         end
+        ending = ending+1;
+        continue;
+    else
+        %unsuccessful, so goes back one
+        ending = ending-1; 
     end
+    break;    
 end
-                    
-                
+% removes scanned selection from string
+str = [str(1:pos),str(ending+1:length(str))];
+end
 
 %% removes identity statements
 % identity statements such as +0, *1, and ^1 clutter up the expression
-% unnecessarily. this portion of code removes them
+% unnecessarily. this portion of code removes them with a series of simple
+% strrep statements. It runs twice in case evaluating one identity
+% statement brings about another
 
 for j = 1:2
     str = strrep(str,'*1','');
     str = strrep(str,'1*','');
-    str = strrep(str,'^1','');
+    str = strrep(str,'^{1}','');   % note that this is in brackets. this is to make latexify easier for Ben.
     str = strrep(str,'+0','');
     str = strrep(str,'0+','');
     str = strrep(str,'0-','-');
